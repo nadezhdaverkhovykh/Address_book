1b761edf2d19b544e7fea73b6e3e0941787fa47f
nadezhdaverkhovykh <verhyshka.mail@mail.ru>
2024-06-16 02:40:00 +0300

    first

---
+++ script.js
@@ -0,0 +1,101 @@
+import { contacts } from "./data.js";
+// console.log(contacts);
+// const searchBar = document.querySelector(".contact_form");
+// searchBar.addEventListener("submit", function (event) {
+//   event.preventDefault();
+// });
+// document.querySelector(".button4").addEventListener("submit", function () {
+//   findContact();
+// });
+// function findContact() {
+//   const searchBar = document.querySelector(".contact_form").value.toLowerCase();
+//   searchBar.preventDefault();
+//
+//   contacts.forEach((el) => {
+//     if (
+//       el.name.toLowerCase().includes(searchBar) ||
+//       el.surname.toLowerCase().includes(searchBar)
+//     ) {
+//       const div = document.createElement("div");
+//       div.className = "contact_wrapper";
+
+//
+//
+//     }
+//   });
+// }
+
+const contactsDiv = document.querySelector(".contactsDiv");
+
+function render(arr) {
+  contactsDiv.innerHTML = "";
+  arr.forEach((el) => {
+    const div = document.createElement("div");
+    div.className = "contact_wrapper";
+    div.innerHTML = `
+      <img src="./src/female_icon.png" alt="" />
+      <p class="contact_name">${el.name} ${el.surname}</p>
+      <p class="contact_about">${el.about}</p>
+      <p class="gender">${el.gender}</p>
+      <p class="contact_number">${el.phone}</p>
+      <p class="contact_email">${el.mail}</p>
+    `;
+    contactsDiv.append(div);
+  });
+}
+
+render(contacts);
+
+document.querySelector(".button2").addEventListener("click", function () {
+  sort(contacts);
+  render(contacts);
+});
+
+function sort(arr) {
+  arr.sort((a, b) => {
+    return a.name.localeCompare(b.name);
+  });
+}
+
+// document.querySelector(".newest").addEventListener("click", function () {
+//   sortByDate(contacts);
+//   render(contacts);
+// });
+
+// function sortByDate(arr) {
+//   arr.sort((a, b) => {
+//     if (a.creationDate < b.creationDate) {
+//       return -1;
+//     }
+//     if (a.creationDate > b.creationDate) {
+//       return 1;
+//     }
+//     return 0;
+//   });
+// }
+
+// sortByDate(contacts);
+
+// console.log(contacts);
+
+document.querySelector(".button1").addEventListener("click", function () {
+  sortByGender(contacts);
+  render(contacts);
+});
+
+function sortByGender(arr) {
+  arr.sort((a, b) => {
+    return a.gender.localeCompare(b.gender);
+  });
+}
+
+// document.querySelector(".male").addEventListener("click", function () {
+//   sortByGender2(contacts);
+//   render(contacts);
+// });
+
+// function sortByGender2(arr) {
+//   arr.sort((a, b) => {
+//     return b.gender.localeCompare(a.gender);
+//   });
+// }
