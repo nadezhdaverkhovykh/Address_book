e6927d3f02d89756d592b1d6d1efce60fe520ebc
nadezhdaverkhovykh <verhyshka.mail@mail.ru>
2024-07-17 21:45:13 +0300

    NewChanges

--- script.js
+++ script.js
@@ -1,82 +1,124 @@
-import { contacts } from "./data.js";
-// console.log(contacts);
-// const searchBar = document.querySelector(".contact_form");
-// searchBar.addEventListener("submit", function (event) {
-//   event.preventDefault();
-// });
-// document.querySelector(".button4").addEventListener("submit", function () {
-//   findContact();
-// });
-// function findContact() {
-//   const searchBar = document.querySelector(".contact_form").value.toLowerCase();
-//   searchBar.preventDefault();
-//
-//   contacts.forEach((el) => {
-//     if (
-//       el.name.toLowerCase().includes(searchBar) ||
-//       el.surname.toLowerCase().includes(searchBar)
-//     ) {
-//       const div = document.createElement("div");
-//       div.className = "contact_wrapper";
-
-//
-//
-//     }
-//   });
-// }
-
+const BASE_URL = "http://localhost:3000/contacts";
+const inputSearch = document.querySelector(".list");
 const contactsDiv = document.querySelector(".contactsDiv");
+const changeGender = document.querySelector(".select_gender");
+const sortSelect = document.querySelector(".sort");
+const openModal = document.querySelector(".button3");
+const modal = document.querySelector(".modal");
+const modalForm = document.querySelector(".modal_form");
 
-function render(arr) {
+//Превью только что загруженной картинки
+const preImage = document.querySelector("#profile_pic");
+const img = document.querySelector("#img");
+preImage.addEventListener("change", (event) => {
+  const file = event.target.files[0];
+  console.log(file);
+  if (file) {
+    const reader = new FileReader();
+    reader.onload = (e) => console.log(e.target.result);
+    img.src = URL.createObjectURL(file);
+  }
+});
+
+modalForm.addEventListener("submit", async (evt) => {
+  evt.preventDefault();
+  const contactData = new FormData(modalForm);
+  console.log(contactData);
+
+  console.log(contactData);
+  const post = await postData("http://localhost:3000/contacts", contactData);
+  if (post) {
+    const query =
+      "?search=" +
+      inputSearch.value +
+      "&gender=" +
+      changeGender.value +
+      "&filter=" +
+      sortSelect.value;
+    const contactData = await fetchData(
+      "http://localhost:3000/contacts" + query
+    );
+    render(contactData);
+    modal.close();
+  }
+});
+openModal.addEventListener("click", () => {
+  modal.showModal();
+});
+
+modal.addEventListener("click", (event) => {
+  if (event.target === modal) modal.close();
+});
+
+async function postData(url, data) {
+  try {
+    console.log(data);
+    const response = await fetch(url, {
+      method: "POST",
+
+      body: data,
+    });
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    const data2 = await response.json();
+    console.log(data2);
+    return data2;
+  } catch (error) {
+    console.log("Error");
+  }
+}
+
+async function fetchData(url, query = "") {
+  try {
+    const response = await fetch(url + query);
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    const data = await response.json();
+    return data.data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function handler() {
+  const query =
+    "?search=" +
+    inputSearch.value +
+    "&gender=" +
+    changeGender.value +
+    "&filter=" +
+    sortSelect.value;
+  const contactData = await fetchData("http://localhost:3000/contacts" + query);
+  render(contactData);
+}
+
+inputSearch.addEventListener("input", handler);
+changeGender.addEventListener("change", handler);
+sortSelect.addEventListener("change", handler);
+
+function render(contactData) {
   contactsDiv.innerHTML = "";
-  arr.forEach((el) => {
+  contactData.forEach((data) => {
     const div = document.createElement("div");
     div.className = "contact_wrapper";
-    div.innerHTML = `
-      <img src="./src/female_icon.png" alt="" />
-      <p class="contact_name">${el.name} ${el.surname}</p>
-      <p class="contact_about">${el.about}</p>
-      <p class="gender">${el.gender}</p>
-      <p class="contact_number">${el.phone}</p>
-      <p class="contact_email">${el.mail}</p>
-    `;
+    console.log(data);
+    const image = data.image
+      ? `
+    <img src="http://localhost:3000/${data.image}" width="70px" height="70px" />
+    `
+      : data.gender === "female"
+      ? '<img src="./src/female_icon.png" alt="Female Icon" width="70px" height="70px" />'
+      : '<img src="./src/male_icon.png" alt="Calendar Icon" width="70px" height="70px" />';
+    div.innerHTML = `${image}
+        <p class="contact_name">${data.name} ${data.surname}</p>
+      <p class="contact_about">${data.about}</p>
+      <p class="gender">${data.gender}</p>
+      <p class="contact_number">${data.phone}</p>
+      <p class="contact_email">${data.mail}</p>`;
     contactsDiv.append(div);
   });
 }
 
-render(contacts);
-
-document.querySelector(".button2").addEventListener("click", function () {
-  sort(contacts);
-  render(contacts);
-});
-
-function sort(arr) {
-  arr.sort((a, b) => {
-    return a.name.localeCompare(b.name);
-  });
-}
-
-const selectElement = document.querySelector(".female");
-const selectElement2 = document.querySelector(".male");
-document.querySelector(".button1").addEventListener("click", function () {
-  sortByGender(contacts);
-  render(contacts);
-});
-
-function sortByGender(arr) {
-  arr.sort((a, b) => {
-    return a.gender.localeCompare(b.gender);
-  });
-}
-
-// document.querySelector(".male").addEventListener("click", function () {
-//   sortByGender2(contacts);
-//   render(contacts);
-// });
-
-// function sortByGender2(arr) {
-//   arr.sort((a, b) => {
-//     return b.gender.localeCompare(a.gender);
-//   });
-// }
+handler();
